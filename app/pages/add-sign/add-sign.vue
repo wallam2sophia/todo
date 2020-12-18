2a3306825cb7bdd82fe0a592524269b3c3f09018
郭修琼 <guoxiuqiong@pwrd.com>
2020-12-18 17:35:24 +0800

    新增打开页面

---
+++ app/pages/add-sign/add-sign.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="add-sign">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style scss="add-sign">
+
+</style>
